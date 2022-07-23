66fe588b923b22b9731acea7315e2008fc4f4d87
shdeepak91 <109649172+shdeepak91@users.noreply.github.com>
2022-07-23 19:20:24 +0530

    Add files via upload
    with change till number fabonacci series is getting printed.

--- Question1 - Copy.py
+++ Question1 - Copy.py
@@ -1,12 +1,13 @@
 # fibonacci series using while loop:
 
-number = int(input("enter a number: "))
+number = int(input("till what number you want to print series:"))
 first = 1
 second = 1
 i = 0
 while i <  number:
-    print(first , end= " ")
-    next = first + second 
-    first = second 
-    second = next
+    if(first <=number):
+      print(first , end= " ")
+      next = first + second 
+      first = second 
+      second = next
     i += 1
